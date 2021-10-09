c5135845497fe5a3a13f7c662f6548bb1488c8e9
Servando <swyswy214@gmail.com>
2021-10-09 15:10:39 -0500

    Higher-order mapping operators (no nested observables)
    An observable that emits an observable is called a higher order observable.

--- APM-Start/src/app/suppliers/supplier.service.ts
+++ APM-Start/src/app/suppliers/supplier.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of } from 'rxjs';
+import { Supplier } from './supplier';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
+
+  supplierWithMaP$ = of(1, 5, 8).pipe(
+    map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  )
 
   constructor(private http: HttpClient) { }
 
